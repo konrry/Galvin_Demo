5fb61c24ca41bfc0cb4a8dd7722fa471d11be411
chuqiang <chuqiang@lvmama.com>
2017-11-20 15:24:21 +0800

    清理代码

--- Spring_Demo/src/main/java/net/galvin/spring/aop/HelloService.java
+++
@@ -1,9 +0,0 @@
-package net.galvin.spring.aop;
-
-public class HelloService {
-
-    public void hello(){
-        System.out.println("---------------------------------- hello ----------------------------------");
-    }
-
-}
